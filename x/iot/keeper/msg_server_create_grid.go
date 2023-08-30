c3fed9f27a41510d8017c61e2c74f3654147c178
Tina Aliakbarpour <51127383+tinaaliakbarpour@users.noreply.github.com>
2023-08-30 16:39:13 +0330

    add condition for creating a grid

--- x/iot/keeper/msg_server_create_grid.go
+++ x/iot/keeper/msg_server_create_grid.go
@@ -25,7 +25,7 @@ func (k msgServer) CreateGrid(goCtx context.Context, msg *types.MsgCreateGrid) (
 		if grid.Name == g.Name {
 			return nil, sdkerrors.Wrap(types.ErrorDuplicate, fmt.Sprintf("key %s already exist", msg.Name))
 		} else if grid.CenterLat == g.CenterLat && grid.CenterLon == g.CenterLon {
-			return nil, sdkerrors.Wrap(types.ErrorDuplicate, fmt.Sprintf("key with this location: %d, %d already exist", msg.Name, msg.CenterLat, msg.CenterLon))
+			return nil, sdkerrors.Wrap(types.ErrorDuplicate, fmt.Sprintf("key with this location: %d, %d already exist", msg.CenterLat, msg.CenterLon))
 		}
 	}
 	id := k.AppendGrid(
